353367c0be02138d3a28f9e91381b2c94bc22cd6
Alex Andru <alex007d@gmail.com>
2023-10-15 19:56:59 +0200

    Remove unused variable 'action'

--- app/index.js
+++ app/index.js
@@ -1,14 +1,13 @@
 import Table from "./Table/Table.js";
 import Game from "./Game/Game.js";
-
-const table = new Table(10, 10);
+const table = new Table(20, 20);
 
 const game = new Game(table);
 
 const startButton = document.querySelector(".game__button");
 
 startButton.addEventListener("click", () => {
-  const action = setInterval(() => {
+  setInterval(() => {
     game.nextGameSequence();
   }, 500);
 });
