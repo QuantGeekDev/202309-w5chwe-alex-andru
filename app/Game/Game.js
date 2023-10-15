0384147eb600a326f6acaf9dd8595fb8b35005b4
Alex Andru <alex007d@gmail.com>
2023-10-15 19:07:01 +0200

    Add renderer, webRenderer

--- app/Game/Game.js
+++ app/Game/Game.js
@@ -1,13 +1,18 @@
+import Renderer from "../Renderer/Renderer.js";
 class Game {
   table;
+  renderer;
 
   constructor(table) {
     this.table = table;
+    this.renderer = new Renderer(this.table.data);
   }
 
   nextGameSequence() {
+    this.renderer.clearWebTable();
     this.table.countAliveNeighbors();
     this.table.updateCells();
+    this.renderer.displayWebTable(this.table.data);
   }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 import Table from "./Table/Table.js";
 import Game from "./Game/Game.js";
 
-const table = new Table(6, 6);
+const table = new Table(10, 10);
 
 const game = new Game(table);
 
-game.nextGameSequence();
+const gameButton = document.querySelector(".game__button");
+const buttonListener = gameButton.addEventListener("click", () => {
+  game.nextGameSequence();
+  game.renderer.displayWebTable();
+});
