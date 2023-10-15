83d10736515fe15a77fe1d50ca15de2edb8497b3
Alex Andru <alex007d@gmail.com>
2023-10-15 14:36:47 +0200

    Add setStatus(), getStatus()

--- app/Cell/Cell.test.js
+++ app/Cell/Cell.test.js
@@ -8,5 +8,54 @@ describe("Given an class Cell", () => {
 
       expect(cell.isAlive).toBe(expectedIsAlive);
     });
+
+    describe("When called with increaseLiveNeighborsAmount()", () => {
+      test("Then liveNeighborsAmount is 1", () => {
+        const expectedLiveNeighborsAmount = 1;
+        const cell = new Cell(true);
+        cell.increaseLiveNeighborsAmount();
+        expect(cell.liveNeighborsAmount).toBe(expectedLiveNeighborsAmount);
+      });
+    });
+  });
+
+  describe("When called with resetLiveNeighborsAmount()", () => {
+    test("Then liveNeighborsAmount is 0", () => {
+      const expectedLiveNeighborsAmount = 0;
+      const cell = new Cell(true);
+      cell.liveNeighborsAmount = 5;
+      cell.resetLiveNeighborsAmount();
+      expect(cell.liveNeighborsAmount).toBe(expectedLiveNeighborsAmount);
+    });
+  });
+
+  describe("When isAlive is set to true", () => {
+    test("Then getStatus() returns true", () => {
+      const expectedIsAlive = true;
+      const cell = new Cell(true);
+      const cellIsAlive = cell.isAlive;
+
+      expect(cellIsAlive).toBe(expectedIsAlive);
+    });
+  });
+
+  describe("When isAlive is set to false", () => {
+    test("Then getStatus() returns false", () => {
+      const expectedIsAlive = false;
+      const cell = new Cell(false);
+      const cellIsAlive = cell.isAlive;
+
+      expect(cellIsAlive).toBe(expectedIsAlive);
+    });
+  });
+
+  describe("When setStatus(true) is called", () => {
+    test("Then .isAlive returns true", () => {
+      const expectedIsAlive = true;
+      const cell = new Cell(false);
+      cell.setStatus(true);
+
+      expect(cell.isAlive).toBe(expectedIsAlive);
+    });
   });
 });
